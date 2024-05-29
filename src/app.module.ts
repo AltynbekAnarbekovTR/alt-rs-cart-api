343e294f5b89172ca88e921a0136be560cabc3e4
Altynbek Anarbekov <altynbek_anarbekov@epam.com>
2024-05-29 20:53:38 +0600

    add logs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,34 @@
-import { Module } from '@nestjs/common';
-
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 
 import { CartModule } from './cart/cart.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
+import { Logger } from '@nestjs/common';
 
 @Module({
   imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: process.env.DB_HOST,
+    //   port: parseInt(process.env.DB_PORT, 10),
+    //   username: process.env.DB_USER,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_NAME,
+    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //   synchronize: true,
+    // }),
     AuthModule,
     CartModule,
     OrderModule,
   ],
-  controllers: [
-    AppController,
-  ],
+  controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  private readonly logger = new Logger(AppModule.name);
+
+  async onModuleInit() {
+    this.logger.log('Database connection established successfully.');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,91 @@
 import { NestFactory } from '@nestjs/core';
-
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { Server } from 'http';
+import * as express from 'express';
 import * as helmet from 'helmet';
-
 import { AppModule } from './app.module';
+import { createServer, proxy } from 'aws-serverless-express';
+import { SecretsManager } from 'aws-sdk';
+import { Client } from 'pg';
 
 const port = process.env.PORT || 4000;
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+let cachedServer: Server;
 
+async function getDatabaseCredentials(secretArn: string) {
+  console.log('logo getDatabaseCredentials');
+  const secretsManager = new SecretsManager();
+  const secretValue = await secretsManager
+    .getSecretValue({ SecretId: secretArn })
+    .promise();
+  if ('SecretString' in secretValue) {
+    console.log('logo secretValue', secretValue);
+    return JSON.parse(secretValue.SecretString);
+  }
+  throw new Error('Could not retrieve secret');
+}
+
+async function bootstrap() {
+  console.log('logo start bootstrap');
+  const expressApp = express();
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
   app.enableCors({
     origin: (req, callback) => callback(null, true),
   });
   app.use(helmet());
-
-  await app.listen(port);
+  await app.init();
+  console.log('logo end bootstrap');
+  return createServer(expressApp);
 }
-bootstrap().then(() => {
-  console.log('App is running on %s port', port);
-});
+
+export const handler = async (event, context) => {
+  if (!cachedServer) {
+    console.log('logo Moi event', event);
+    console.log('logo Moi context', context);
+    console.log('logo before cachedServer = await bootstrap();');
+    cachedServer = await bootstrap();
+  }
+  console.log('logo before const dbSecretArn = process.env.DB_SECRET_ARN;');
+  const dbSecretArn = process.env.DB_SECRET_ARN;
+  try {
+    console.log(
+      'logo before const dbCredentials = await getDatabaseCredentials(dbSecretArn);',
+    );
+    const dbCredentials = await getDatabaseCredentials(dbSecretArn);
+    console.log('dbCredentials', dbCredentials);
+    const conf = {
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      user: dbCredentials.username,
+      password: dbCredentials.password,
+      // database: process.env.DB_NAME,
+      database: dbCredentials.dbname,
+    };
+    console.log('logo conf', conf);
+    const client = new Client(conf);
+
+    await client.connect();
+    console.log('logo Successfully connected to the database');
+
+    // Ensure the client is disconnected after request is processed
+    context.callbackWaitsForEmptyEventLoop = false;
+    context.onEnd(() => {
+      client.end();
+    });
+  } catch (error) {
+    console.error('logo Failed to connect to the database:', error);
+  }
+
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
+};
+
+if (require.main === module) {
+  bootstrap()
+    .then(() => {
+      console.log('logo App is running on %s port', port);
+    })
+    .catch(() => console.log('logo bootstrap failed'));
+}
